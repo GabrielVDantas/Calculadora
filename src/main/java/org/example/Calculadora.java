13bf7090ab9334c86a27a88d85498a839fda7c6d
GabrielVDantas <gvdantas2028@gmail.com>
2024-03-20 15:00:29 -0300

    ajustando os comentários

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -1,25 +1,50 @@
 package org.example;
 
+/**
+ * @author Gabriel Viegas Dantas
+ * @version 1.1
+ * @since Release 1.1 da aplicação
+ */
+
 public class Calculadora {
 
-    // Método 1: Recebe dois números inteiros e realiza a soma deles
+    /**
+     *
+     * @param n1, primeiro número inteiro que será usado na soma
+     * @param n2, segundo número inteiro que será usado na soma
+     * @return , o resultado de n1 mais n2
+     */
     public double somaDoisNumerosInteiros(int n1, int n2) {
         return n1 + n2;
     }
 
-    // Método 2: Recebe dois números inteiros e realiza a subtração deles
+    /**
+     *
+     * @param n1, primeiro número inteiro que será usado na subtração
+     * @param n2, segundo número inteiro que será usado na subtração
+     * @return , o resultado de n1 menos n2
+     */
     public double subtraiDoisNumerosInteiros(int n1, int n2) {
         return n1 - n2;
     }
 
-    // Método 3: Recebe dois números inteiros e realiza a multiplicação deles
+    /**
+     *
+     * @param n1, primeiro número inteiro que será usado na multiplicação
+     * @param n2, segundo número inteiro que será usado na multiplicação
+     * @return , o resultado de n1 multiplicado por n2
+     */
     public double multiplicaDoisNumerosInteiros(int n1, int n2) {
         return n1 * n2;
     }
 
-    // Método 4: Recebe dois números inteiros e realiza a divisão deles
+    /**
+     *
+     * @param n1, primeiro número inteiro que será usado na divisão
+     * @param n2, segundo número inteiro que será usado na divisão
+     * @return , o resultado de n1 dividido por n2
+     */
     public double divideDoisNumerosInteiros(int n1, int n2) {
         return (double) n1 / n2;
     }
-
 }
--- src/test/java/org/example/CalculadoraJUnitTest.java
+++ src/test/java/org/example/CalculadoraJUnitTest.java
@@ -1,5 +1,11 @@
 package org.example;
 
+/**
+ * @author Gabriel Viegas Dantas
+ * @version 1.1
+ * @since Release 1.1 da aplicação
+ */
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,24 +17,32 @@ class CalculadoraJUnitTest {
     @Test
     @DisplayName("O método recebe dois números inteiros e soma o segundo ao primeiro")
     void somaDoisNumerosInteiros() {
+//        comparação entre o valor esperado (nesse caso 15)
+//        e o valor que será retornado pelo metodo somaDoisNumerosInteiros ao receber 10 e 5 como parâmetros
         assertEquals(15, calculadora.somaDoisNumerosInteiros(10, 5));
     }
 
     @Test
     @DisplayName("O método recebe dois números inteiros e subtrai o segundo do primeiro")
     void subtraiDoisNumerosInteiros() {
+//        comparação entre o valor esperado (nesse caso 5)
+//        e o valor que será retornado pelo metodo subtraiDoisNumerosInteiros ao receber 10 e 5 como parâmetros
         assertEquals(5, calculadora.subtraiDoisNumerosInteiros(10, 5));
     }
 
     @Test
     @DisplayName("O método recebe dois números inteiros e multiplica o primeiro pelo segundo")
     void multiplicaDoisNumerosInteiros() {
+//        comparação entre o valor esperado (nesse caso 50)
+//        e o valor que será retornado pelo metodo multiplicaDoisNumerosInteiros ao receber 10 e 5 como parâmetros
         assertEquals(50, calculadora.multiplicaDoisNumerosInteiros(10, 5));
     }
 
     @Test
     @DisplayName("O método recebe dois números inteiros e divide o primeiro pelo segundo")
     void divideDoisNumerosInteiros() {
+//        comparação entre o valor esperado (nesse caso 2)
+//        e o valor que será retornado pelo metodo divideDoisNumerosInteiros ao receber 10 e 5 como parâmetros
         assertEquals(2, calculadora.divideDoisNumerosInteiros(10, 5));
     }
 }
