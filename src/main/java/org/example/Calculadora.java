6679c470309d02bfd69d17dd9e44846387f1251c
GabrielVDantas <gvdantas2028@gmail.com>
2024-03-20 15:06:44 -0300

    corrigindo os comentários

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -1,38 +1,50 @@
-
-//    @author Gabriel Viegas Dantas
-//    @version 1.0
-//    @since Release 1.0 da aplicação
-
 package org.example;
+
+/**
+ * @author Gabriel Viegas Dantas
+ * @version 1.0
+ * @since Release 1.0 da aplicação
+ */
 
 public class Calculadora {
 
-    // @param n1, primeiro número inteiro da soma
-    // @param n2, segundo número inteiro da soma
-    // @return , resultado de n1 mais n2
+    /**
+     *
+     * @param n1, primeiro número inteiro que será usado na soma
+     * @param n2, segundo número inteiro que será usado na soma
+     * @return , resultado de n1 mais n2
+     */
     public double somaDoisNumerosInteiros(int n1, int n2) {
         return n1 + n2;
     }
 
-    // @param n1, primeiro número inteiro da subtração
-    // @param n2, segundo número inteiro da subtração
-    // @return , resultado de n1 menos n2
+    /**
+     *
+     * @param n1, primeiro número inteiro que será usado na subtração
+     * @param n2, segundo número inteiro que será usado na subtração
+     * @return , resultado de n1 menos n2
+     */
     public double subtraiDoisNumerosInteiros(int n1, int n2) {
         return n1 - n2;
     }
 
-    // @param n1, primeiro número inteiro da multiplicação
-    // @param n2, segundo número inteiro da multiplicação
-    // @return , resultado de n1 vezes n2
+    /**
+     *
+     * @param n1, primeiro número inteiro que será usado na multiplicação
+     * @param n2, segundo número inteiro que será usado na multiplicação
+     * @return , resultado de n1 multiplicado por n2
+     */
     public double multiplicaDoisNumerosInteiros(int n1, int n2) {
         return n1 * n2;
     }
 
-    // @param n1, primeiro número inteiro da divisão
-    // @param n2, segundo número inteiro da divisão
-    // @return , resultado de n1 divido por n2
+    /**
+     *
+     * @param n1, primeiro número inteiro que será usado na divisão
+     * @param n2, segundo número inteiro que será usado na divisão
+     * @return , resultado de n1 dividido por n2
+     */
     public double divideDoisNumerosInteiros(int n1, int n2) {
         return (double) n1 / n2;
     }
-
 }
