2b36ec8187606ec82c2a8702c8e6422a157d792d
GabrielVDantas <gvdantas2028@gmail.com>
2024-03-20 09:53:54 -0300

    calculadora sem junit

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Calculadora {
+
+    public double somaDoisNumerosInteiros(double n1, double n2) {
+        return n1 + n2;
+    }
+
+    public double subtraiDoisNumerosInteiros(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    public double multiplicaDoisNumerosInteiros(double n1, double n2) {
+        return n1 * n2;
+    }
+
+    public double divideDoisNumerosInteiros(double n1, double n2) {
+        return n1 / n2;
+    }
+
+}
